5da286a47d6d86be3f987be030ee2dbedf0eb6d0
Nathan <nathan.agu.2103@gmail.com>
2025-03-20 14:38:20 +0100

    feat: dynamicGetById

--- src/routes/dynamicRoutes.js
+++ src/routes/dynamicRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const dynamicController = require('../controllers/dynamicController');
 
 router.get('/:tableName', dynamicController.getTableData);
+router.get('/:tableName/:id', dynamicController.getByIdTableData);
 router.post('/:tableName', dynamicController.addTableData);
 router.put('/:tableName/:id', dynamicController.putTableData);
-router.delete('/:tableName/:id', dynamicController.deleteTableData); 
+router.delete('/:tableName/:id', dynamicController.deleteTableData);
 
 module.exports = router;
