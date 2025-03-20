4e955763affaaae1d2eecc5d561663f0754949d9
Nathan <nathan.agu.2103@gmail.com>
2025-03-20 14:42:58 +0100

    update: get /all

--- src/routes/dynamicRoutes.js
+++ src/routes/dynamicRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dynamicController = require('../controllers/dynamicController');
 
-router.get('/:tableName', dynamicController.getTableData);
+router.get('/:tableName/all', dynamicController.getTableData);
 router.get('/:tableName/:id', dynamicController.getByIdTableData);
 router.post('/:tableName', dynamicController.addTableData);
 router.put('/:tableName/:id', dynamicController.putTableData);
